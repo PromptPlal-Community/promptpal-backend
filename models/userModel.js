66800ecb12b8b23821f74fc13a08e6761d39f833
AdeproCode <waleayomideadetokun@gmail.com>
2025-10-07 12:52:41 +0100

    Updated

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
-  googleId: { type: String, required: true, unique: true },
+  googleId: { type: String, unique: true },
   name: {
     type: String,
     default: "",
